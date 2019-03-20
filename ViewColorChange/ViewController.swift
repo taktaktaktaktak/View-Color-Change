690162701fb8454a2270f7cbf687f93224549985
dit000 <dit000@dit000.local>
2019-03-20 15:29:26 +0900

    Add isHidden

--- ViewColorChange/ViewController.swift
+++ ViewColorChange/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var RedView: UIView!
+    @IBOutlet weak var BlueView: UIView!
+    @IBOutlet weak var GreenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.lightGray
     }
 
-
+    @IBAction func RedButton(_ sender: Any) {
+        RedView.isHidden = true
+        //RedView.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func BlueButton(_ sender: Any) {
+        BlueView.isHidden = true
+        //BlueView.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func GreenButton(_ sender: Any) {
+        GreenView.isHidden = true
+        //GreenView.backgroundColor = UIColor.white
+    }
+    
 }
 
